7e39244dbcd076bc5e73696a6516e4efffd66e22
moeelnady <mohamedelnady007@gmail.com>
2026-03-27 20:08:51 +0200

    spring dependencies added

---
+++ HiveBox/src/main/java/com/example/HiveBox/HiveBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.HiveBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveBoxApplication.class, args);
+	}
+
+}
---
+++ HiveBox/src/test/java/com/example/HiveBox/HiveBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HiveBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
