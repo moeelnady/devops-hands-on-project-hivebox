673f22379fe1d51faf6953674c769794bebb94d5
moeelnady <mohamedelnady007@gmail.com>
2026-03-27 20:26:40 +0200

    message added

--- HiveBox/src/main/java/com/example/HiveBox/HiveBoxApplication.java
+++ HiveBox/src/main/java/com/example/HiveBox/HiveBoxApplication.java
@@ -2,12 +2,16 @@ package com.example.HiveBox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HiveBoxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HiveBoxApplication.class, args);
+		ConfigurableApplicationContext context =
+				SpringApplication.run(HiveBoxApplication.class, args);
+		System.out.println("My current Application Version is : v0.0.1");
+		context.close();
 	}
 
 }
